5365c9fa0f2cd7f02757d1f9e3b7621ba2e64ebe
Jerry Plotke <jerry.plotke@gmail.com>
2021-06-08 13:22:09 -0500

    added readme outline

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,45 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    return `## ${data.title}
 
+    ## Table of Contents:
+    - Description
+    - Installation
+    - Usage
+    - License
+    - Contributing
+    - Tests
+    - Questions
+
+    ## Description:
+
+    ${data.description}
+
+    ## Installation:
+
+    ${data.installation}
+
+    ## Usage:
+
+    ${data.usage}
+
+    ## License:
+
+    ${data.license}
+
+    ## Contributing:
+
+    ${data.contributing}
+
+    ## Tests:
+
+    ${data.tests}
+
+    ## Questions:
+
+    You can contact me with any questions at the following email address: ${data.email}
+    You can find me on GitHub at https://github.com/${data.username}
 `;
 }
 
