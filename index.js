5eb66dd40fe76ea21f59eb329b6776e52471955b
Jerry Plotke <jerry.plotke@gmail.com>
2021-06-08 04:15:17 -0500

    added questions and prompts

--- index.js
+++ index.js
@@ -1,12 +1,68 @@
 // TODO: Include packages needed for this application
-const package = require('inquirer')
+const inquirer = require('inquirer');
 
-console.log('package is:', package)
+// console.log('package is:', package)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your github username?',
+    'What is your email address?',
+    'What is your project name?',
+    'Please write a description of your project.',
+    'What kind of license should your project have?',
+    'What command should be run to install the dependencies?',
+    'What command should be run to run tests?',
+    'What does the user need to know about using the repository?',
+    'What does the user need to know about contributing to the repository?',
+];
 
-// TODO: Create a function to write README file
+inquirer
+    .prompt([{
+            type: 'input',
+            message: questions[0],
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'project name',
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'dependencies',
+        },
+        {
+            type: 'input',
+            message: questions[6],
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'contributing',
+        },
+    ])
+    // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
